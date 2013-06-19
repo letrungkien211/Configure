cccd7ee30a2dc163b8547b0af5b61b8877ce2eb1
letrungkien211 <letrungkien211@gmail.com>
2013-06-19 20:29:08 +0900

    Perfect

---
+++ my.bash
@@ -0,0 +1,10 @@
+export GITREMOTE=https://letrungkien211@github.com/letrungkien211
+###################################################
+# PATH
+export PATH=$PATH
+# LDCONFIG
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+# PKGCONFIG
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+
+
